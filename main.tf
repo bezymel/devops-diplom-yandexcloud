8863255f8547ecb217b8ea9faee6d5c5a681864d
Denis <129361495+bezymel@users.noreply.github.com>
2025-01-16 21:55:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,21 +5,15 @@ terraform {
       version = "~> 0.69.0"
     }
   }
-
-  backend "s3" {
-    bucket = "my-bucket-name"                                 
-    key    = "terraform/state"          
-    region = "ru-central1"              
-  }
+  required_version = ">= 0.13"
 }
-
-provider "yandex" {
-  service_account_key = file("/home/bezumel/authorized_key.json")  
-  cloud_id           = "b1g8tssboq6kq6qsl5pb" 
-  folder_id          = "b1gaadh5jrnspg1gklri"                   
 }
 
 resource "yandex_storage_bucket" "my_bucket" {
-  name     = "my-bucket-name"   
-  location = "ru-central1"      
+  name     = "my-bucket-name"
+  location = "ru-central1"
+}
+
+output "bucket_id" {
+  value = yandex_storage_bucket.my_bucket.id
 }
