e81abb6f0af9aed8007ec710099a3f5364508e45
Denis <129361495+bezymel@users.noreply.github.com>
2025-01-16 22:55:49 +0300

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "my-bucket-name"
-  value       = yandex_storage_bucket.my_bucket.name
-}
-
-output "bucket_location" {
-  description = "terraform/state"
-  value       = yandex_storage_bucket.my_bucket.location
-}
