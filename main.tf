54a41ca9979757c075b334746280c011b2b314be
Denis <129361495+bezymel@users.noreply.github.com>
2025-01-17 03:14:16 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~> 0.69.0"
+      version = "~> 0.133"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.0"
 }
 
 provider "yandex" {
-  service_account_key_file = file("/home/bezumel/authorized_key.json")
+  service_account_key_file = file("/home/bezumel/diplom/authorized_key.json")
 #  token              = var.token
   folder_id          = var.folder_id
   zone               = var.zone
@@ -17,5 +17,9 @@ provider "yandex" {
 
 resource "yandex_iam_service_account" "my_service_account" {
   name        = "bezumel"
-  description = "Description of the service account."           
+  description = "Description of the service account."
+}
+
+resource "yandex_storage_bucket" "my_bucket" {
+  bucket = "my-bucket-bezumel"
 }
