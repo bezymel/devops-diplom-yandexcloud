104aad557d242a14cc3140496cd6729edbf7e2fd
Denis <129361495+bezymel@users.noreply.github.com>
2025-01-17 17:34:56 +0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,8 +4,21 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+
+   backend "s3" {
+    endpoint                    = "https://storage.yandexcloud.net"
+    bucket                      = "diplom-project-nemcev"
+    region                      = "ru-central1"
+    key                         = "terraform.tfstate"
+    access_key                  = "my_access_key"
+    secret_key                  = "my_secret_key"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+
   required_version = ">=1.4"
   }
+
 provider "yandex" {
   token = var.token
   cloud_id = var.cloud_id
