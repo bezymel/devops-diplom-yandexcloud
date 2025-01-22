9877167508f273d67a8f0e3658312c76833f87c7
Denis <129361495+bezymel@users.noreply.github.com>
2025-01-22 14:45:07 +0300

    Create main.tf

---
+++ terraform2/main.tf
@@ -0,0 +1,75 @@
+#Конфиграция control-plane ноды
+
+resource "yandex_vpc_subnet" "public_subnet" {
+  count = length(var.public_subnet_zones)
+  name  = "${var.public_subnet_name}-${var.public_subnet_zones[count.index]}"
+  v4_cidr_blocks = [
+    cidrsubnet(var.public_v4_cidr_blocks[0], 4, count.index)
+  ]
+  zone       = var.public_subnet_zones[count.index]
+  network_id = yandex_vpc_network.my_vpc.id
+}
+
+resource "yandex_compute_instance" "control-plane" {
+  name            = var.control_plane_name
+  platform_id     = var.platform
+  resources {
+    cores         = var.control_plane_core
+    memory        = var.control_plane_memory
+    core_fraction = var.control_plane_core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+      size     = var.control_plane_disk_size
+    }
+  }
+
+  scheduling_policy {
+    preemptible = var.scheduling_policy
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.public_subnet[0].id
+    nat       = var.nat
+  }
+
+  metadata = {
+    user-data = "${file("/home/bezumel/Diplom1/terraform/cloud-init.yaml")}"
+ }
+}
+
+#Конфигурация worker нод:
+
+resource "yandex_compute_instance" "worker" {
+  count           = var.worker_count
+  name            = "worker-node-${count.index + 1}"
+  platform_id     = var.worker_platform
+  zone = var.public_subnet_zones[count.index]
+  resources {
+    cores         = var.worker_cores
+    memory        = var.worker_memory
+    core_fraction = var.worker_core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+      size     = var.worker_disk_size
+    }
+  }
+
+    scheduling_policy {
+    preemptible = var.scheduling_policy
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.public_subnet[count.index].id
+    nat       = var.nat
+  }
+
+  metadata = {
+    user-data = "${file("/home/bezumel/Diplom1/terraform/cloud-init.yaml")}"
+ }
+}
