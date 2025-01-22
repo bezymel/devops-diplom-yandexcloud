26b447267566896bc4a804126000e365b82faafa
Denis <129361495+bezymel@users.noreply.github.com>
2025-01-22 14:00:23 +0300

    Create main.tf

---
+++ terraform1/main.tf
@@ -0,0 +1,15 @@
+#Создаем VPC с подсетями в разных зонах доступности
+
+resource "yandex_vpc_network" "my_vpc" {
+  name = var.VPC_name
+}
+
+resource "yandex_vpc_subnet" "public_subnet" {
+  count = length(var.public_subnet_zones)
+  name  = "${var.public_subnet_name}-${var.public_subnet_zones[count.index]}"
+  v4_cidr_blocks = [
+    cidrsubnet(var.public_v4_cidr_blocks[0], 4, count.index)
+  ]
+  zone       = var.public_subnet_zones[count.index]
+  network_id = yandex_vpc_network.my_vpc.id
+}
