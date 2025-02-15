55604a42949521f99ce46bd9498a2874bd2c1593
Denis <129361495+bezymel@users.noreply.github.com>
2025-02-15 23:37:36 +0300

    Update main.tf

--- terraform2/main.tf
+++ terraform2/main.tf
@@ -1,7 +1,178 @@
+#СоздаeM сервисный аккаунт
+
+resource "yandex_iam_service_account" "ww" {
+  name = var.ww_name
+  description = "Description of the service account."
+}
+
+#Добавляем права storage.admin
+
+resource "yandex_resourcemanager_folder_iam_member" "ww-admin" {
+  folder_id = var.folder_id
+  role      = "storage.admin"
+  member    = "serviceAccount:${yandex_iam_service_account.ww.id}"
+}
+
+#Создаем ключ доступа к нашему хранилищу
+
+resource "yandex_iam_service_account_static_access_key" "ww-static-key" {
+  service_account_id = yandex_iam_service_account.ww.id
+  description        = "static access key for object storage"
+}
+
+
+
+#Создаем VPC с подсетями в разных зонах доступности
+
+resource "yandex_vpc_network" "my_vpc" {
+  name = var.VPC_name
+}
+
+resource "yandex_vpc_subnet" "mysubnet-a" {
+  name = "mysubnet-a"
+  v4_cidr_blocks = ["10.5.0.0/16"]
+  zone           = "ru-central1-a"
+  network_id     = var.yandex_vpc_network
+}
+
+resource "yandex_vpc_subnet" "mysubnet-b" {
+  name = "mysubnet-b"
+  v4_cidr_blocks = ["10.6.0.0/16"]
+  zone           = "ru-central1-b"
+  network_id     = var.yandex_vpc_network
+}
+
+resource "yandex_vpc_subnet" "mysubnet-d" {
+  name = "mysubnet-d"
+  v4_cidr_blocks = ["10.7.0.0/16"]
+  zone           = "ru-central1-d"
+  network_id     = var.yandex_vpc_network
+}
+
+
+#Создание кластера
+
+locals {
+  folder_id   = var.folder_id
+}
+
+resource "yandex_kubernetes_cluster" "k8s-regional" {
+  name = "k8s-regional"
+  network_id = var.yandex_vpc_network
+  master {
+    master_location {
+      zone      = "ru-central1-a"
+      subnet_id = yandex_vpc_subnet.mysubnet-a.id
+    }
+    master_location {
+      zone      = "ru-central1-b"
+      subnet_id = yandex_vpc_subnet.mysubnet-b.id
+    }
+    master_location {
+      zone      = "ru-central1-d"
+      subnet_id = yandex_vpc_subnet.mysubnet-d.id
+    }
+    security_group_ids = [yandex_vpc_security_group.regional-k8s-sg.id]
+  }
+  service_account_id      = yandex_iam_service_account.ww.id
+  node_service_account_id = yandex_iam_service_account.ww.id
+  depends_on = [
+    yandex_resourcemanager_folder_iam_member.k8s-clusters-agent,
+    yandex_resourcemanager_folder_iam_member.vpc-public-admin,
+    yandex_resourcemanager_folder_iam_member.images-puller,
+    yandex_resourcemanager_folder_iam_member.encrypterDecrypter
+  ]
+  kms_provider {
+    key_id = var.my-bucket-encryption-key
+  }
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "k8s-clusters-agent" {
+  # Сервисному аккаунту назначается роль "k8s.clusters.agent".
+  folder_id = local.folder_id
+  role      = "k8s.clusters.agent"
+  member    = "serviceAccount:${yandex_iam_service_account.ww.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "vpc-public-admin" {
+  # Сервисному аккаунту назначается роль "vpc.publicAdmin".
+  folder_id = local.folder_id
+  role      = "vpc.publicAdmin"
+  member    = "serviceAccount:${yandex_iam_service_account.ww.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "images-puller" {
+  # Сервисному аккаунту назначается роль "container-registry.images.puller".
+  folder_id = local.folder_id
+  role      = "container-registry.images.puller"
+  member    = "serviceAccount:${yandex_iam_service_account.ww.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "encrypterDecrypter" {
+  # Сервисному аккаунту назначается роль "kms.keys.encrypterDecrypter".
+  folder_id = local.folder_id
+  role      = "kms.keys.encrypterDecrypter"
+  member    = "serviceAccount:${yandex_iam_service_account.ww.id}"
+}
+
+resource "yandex_kms_symmetric_key" "kms-key" {
+  # Ключ Yandex Key Management Service для шифрования важной информации, такой как пароли, OAuth-токены и SSH-ключи.
+  name              = "kms-key"
+  default_algorithm = "AES_128"
+  rotation_period   = "8760h" # 1 год.
+}
+
+resource "yandex_vpc_security_group" "regional-k8s-sg" {
+  name        = "regional-k8s-sg"
+  description = "Правила группы обеспечивают базовую работоспособность кластера Managed Service for Kubernetes. Примените ее к кластеру и группам узлов."
+  network_id  = var.yandex_vpc_network
+  ingress {
+    protocol          = "TCP"
+    description       = "Правило разрешает проверки доступности с диапазона адресов балансировщика нагрузки. Нужно для работы отказоустойчивого кластера Managed Service for Kubernetes и сервисов балансировщика."
+    predefined_target = "loadbalancer_healthchecks"
+    from_port         = 0
+    to_port           = 65535
+  }
+  ingress {
+    protocol          = "ANY"
+    description       = "Правило разрешает взаимодействие мастер-узел и узел-узел внутри группы безопасности."
+    predefined_target = "self_security_group"
+    from_port         = 0
+    to_port           = 65535
+  }
+  ingress {
+    protocol          = "ANY"
+    description       = "Правило разрешает взаимодействие под-под и сервис-сервис. Укажите подсети вашего кластера Managed Service for Kubernetes и сервисов."
+    v4_cidr_blocks    = concat(yandex_vpc_subnet.mysubnet-a.v4_cidr_blocks, yandex_vpc_subnet.mysubnet-b.v4_cidr_blocks, yandex_vpc_subnet.mysubnet-d.v4_cidr_blocks)
+    from_port         = 0
+    to_port           = 65535
+  }
+  ingress {
+    protocol          = "ICMP"
+    description       = "Правило разрешает отладочные ICMP-пакеты из внутренних подсетей."
+    v4_cidr_blocks    = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+  }
+  ingress {
+    protocol          = "TCP"
+    description       = "Правило разрешает входящий трафик из интернета на диапазон портов NodePort. Добавьте или измените порты на нужные вам."
+    v4_cidr_blocks    = ["0.0.0.0/0"]
+    from_port         = 30000
+    to_port           = 32767
+  }
+  egress {
+    protocol          = "ANY"
+    description       = "Правило разрешает весь исходящий трафик. Узлы могут связаться с Yandex Container Registry, Yandex Object Storage, Docker Hub и т. д."
+    v4_cidr_blocks    = ["0.0.0.0/0"]
+    from_port         = 0
+    to_port           = 65535
+  }
+}
+
 #Конфиграция control-plane ноды
 
 resource "yandex_compute_instance" "control-plane" {
   name            = var.control_plane_name
+  zone            = "ru-central1-a"
   platform_id     = var.platform
   resources {
     cores         = var.control_plane_core
@@ -21,8 +192,8 @@ resource "yandex_compute_instance" "control-plane" {
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.public_subnet[0].id
-    nat       = var.nat
+    subnet_id = var.mysubnet-a  # Используем первый элемент списка
+    nat       = var.nat                      # Включите NAT, если это необходимо
   }
 
   metadata = {
@@ -47,11 +218,10 @@ resource "yandex_compute_instance" "control-plane" {
 
 #Конфигурация worker нод:
 
-resource "yandex_compute_instance" "worker" {
-  count           = var.worker_count
-  name            = "worker-node-${count.index + 1}"
+resource "yandex_compute_instance" "worker_1" {
+  name            = "worker-node-1"
   platform_id     = var.worker_platform
-  zone            = var.public_subnet_zones[count.index]
+  zone            = "ru-central1-b"
   resources {
     cores         = var.worker_cores
     memory        = var.worker_memory
@@ -70,7 +240,7 @@ resource "yandex_compute_instance" "worker" {
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.public_subnet[count.index].id
+    subnet_id = var.mysubnet-b
     nat       = var.nat
   }
 
@@ -92,3 +262,46 @@ resource "yandex_compute_instance" "worker" {
   }
 }
 
+resource "yandex_compute_instance" "worker_2" {
+  name            = "worker-node-2"
+  platform_id     = var.worker_platform2
+  zone            = "ru-central1-d"
+  resources {
+    cores         = var.worker_cores
+    memory        = var.worker_memory
+    core_fraction = var.worker_core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+      size     = var.worker_disk_size
+    }
+  }
+
+  scheduling_policy {
+    preemptible = var.scheduling_policy
+  }
+
+  network_interface {
+    subnet_id = var.mysubnet-d
+    nat       = var.nat
+  }
+
+  metadata = {
+    user-data = <<-EOF
+      #cloud-config
+      ssh_pwauth: true
+      users:
+        - name: bezumel
+          shell: /bin/bash
+          sudo: ['ALL=(ALL) NOPASSWD:ALL']
+          ssh-authorized-keys:
+            - ${var.ssh_public_key}
+          passwd: ${var.bezumel_password}  # Задайте пароль для пользователя
+      runcmd:
+        - echo 'bezumel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/bezumel
+        - chmod 440 /etc/sudoers.d/bezumel
+    EOF
+  }
+}
