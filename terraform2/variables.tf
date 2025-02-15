3fc5c6e399f7c26fa4fb9a81a657b896e1960625
Denis <129361495+bezymel@users.noreply.github.com>
2025-02-15 23:37:13 +0300

    Update variables.tf

--- terraform2/variables.tf
+++ terraform2/variables.tf
@@ -1,3 +1,61 @@
+### cloud vars
+
+variable "folder_id" {
+  type        = string
+  description = "folder_id"
+}
+
+variable "default_zone" {
+  description = "Availability zone for the instances"
+  type        = string
+  default     = "ru-central1-a"
+}
+
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "ww_name" {
+  description = "Service account name"
+  type        = string
+  default     = "ww"
+}
+
+### vpc vars
+
+variable "VPC_name" {
+  type        = string
+  default     = "my-vpc"
+}
+
+#SSH-key
+
+variable "ssh_public_key" {
+  description = "The public SSH key for accessing the instance."
+  type        = string
+}
+
+variable "yandex_vpc_network" {
+  type    = string
+  default = "enpg8eb9p7oonpva34g3"
+}
+
+variable "regional-k8s-sg" {
+  type    = string
+  default = "enperr0p1ei20fl0cm9h"
+}
+
+variable "my-bucket-encryption-key" {
+  type    = string
+  default = "abjdrrn2ov80f23m8j5n"
+}
+
 ### control-plane node vars
 
 variable "control_plane_name" {
@@ -33,7 +91,7 @@ variable "control_plane_disk_size" {
 
 variable "image_id" {
   type        = string
-  default     = "fd8jnll1ou4fv2gil3rv"
+  default     = "fd893ak78u3rh37q3ekn"
 }
 
 variable "scheduling_policy" {
@@ -41,12 +99,13 @@ variable "scheduling_policy" {
   default     = "true"
 }
 
-### worker nodes vars
-
-variable "worker_count" {
-  type        = number
-  default     = "2"
+variable "mysubnet-a" {
+  description = "Пароль для пользователя bezumel"
+  type        = string
+  default     = "e9bc77gbvrrl1fhngoer"
 }
+
+### worker nodes vars
 
 variable "worker_platform" {
   type        = string
@@ -89,4 +148,22 @@ variable "root_password" {
   description = "Пароль для root пользователя"
   type        = string
   sensitive   = true
+}
+
+variable "mysubnet-b" {
+  description = "Пароль для пользователя bezumel"
+  type        = string
+  default     = "e2log9l8u5hprmrkip8n"
+}
+
+variable "mysubnet-d" {
+  description = "Пароль для пользователя bezumel"
+  type        = string
+  default     = "fl8c1m3119ohhc4mjjl9"
+}
+
+
+variable "worker_platform2" {
+  type        = string
+  default     = "standard-v2"
 }
